417b49c88b602289687e1c3461a59c67c944b046
Cristian Alejandro Gomez Perez <cgomez@rosulgbm.dc>
2020-10-18 14:27:30 -0600

    primera integración con Git

---
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCompany.Handlers
+{
+
+
+    public partial class BlobFactoryConfig : BlobFactory
+    {
+
+        public static void Initialize()
+        {
+            // register blob handlers
+            RegisterHandler("ProductosFotografia", "\"dbo\".\"Productos\"", "\"Fotografia\"", new string[] {
+                        "\"IdProducto\""}, "Productos Fotografia", "Productos", "Fotografia");
+        }
+    }
+}
